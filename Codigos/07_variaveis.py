4d01fb4b1decc9a7529eca0715312857ca634190
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-02-14 23:30:48 -0300

    Trabalhando com variáveis

---
+++ Codigos/07_variaveis.py
@@ -0,0 +1,11 @@
+# Variáveis são usadas para armazenar algo na memoria do computador
+# O sinal de = é o operador de atribuição
+
+nome_completo = 'Naruto Uzumaki'
+print(nome_completo)
+
+nome = 'Naruto'
+idade = 21
+maior_de_idade = idade >= 18
+
+print(f'{nome} é maior de idade? {maior_de_idade}')
---
+++ Exercicios/01_ex_variaveis.py
@@ -0,0 +1,13 @@
+nome = 'Naruto'
+Sobrenome = 'Uzumaki'
+idade = 23
+ano_nascimento = 2000
+maior_de_idade = idade >= 18
+altura_metros = 1.7
+
+print('Nome: ', nome)
+print('Sobrenome: ', Sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Quantos metros possui? ', altura_metros)
