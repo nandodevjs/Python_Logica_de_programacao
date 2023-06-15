7c153fd1e215dc61f91805a35e5f31a51efc7cac
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-06-14 23:26:03 -0300

    imc

---
+++ Exercicios/02_ex_calculo_imc.py
@@ -0,0 +1,10 @@
+nome = 'Naruto'
+Sobrenome = 'Uzumaki'
+idade = 23
+ano_nascimento = 2000
+maior_de_idade = idade >= 18
+altura_metros = 1.7
+peso = 87
+imc = peso / altura_metros ** 2
+
+print(f'{nome} possui o IMC de {imc}')
