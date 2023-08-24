ba25263b01a305a1a5168d476fe362351cf0a181
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-08-24 00:42:06 -0300

    Interpolação de strings

---
+++ Codigos/18_interpolacao_strings_.py
@@ -0,0 +1,15 @@
+'''
+Interpolação básica de strings
+
+s       = strings
+d and i = int
+f       = float
+x e X   = Hexadecimal (ABCDEF0123456789)
+'''
+
+nome     = "Playstation 5"
+valor    = 5.0000
+variavel = 'O valor do %s é %.3f' %(nome, valor)
+
+
+print(variavel)
