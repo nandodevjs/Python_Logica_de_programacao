8f3ca8cda82ec018f0731d1033e13c89d3aef99c
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-08-25 00:47:10 -0300

    Create 19_f_strings_2.py

---
+++ Codigos/19_f_strings_2.py
@@ -0,0 +1,15 @@
+"""
+Formatação básica de strings
+
+s - string
+d - int
+f - float
+"""
+
+variavel = 'ABD'
+
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
+print(f'Hexadecimal de 1500 é {1500:08X}')
