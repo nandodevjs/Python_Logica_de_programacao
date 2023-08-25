3ab4b79d6ae9f17c3f695e7a99ae01af7b184de2
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-08-25 01:03:40 -0300

    Create 20_fatiamento_de_strings.py

---
+++ Codigos/20_fatiamento_de_strings.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+
+Fatiamento [i:f:p]
+
+I = a partir de
+f = finalize aqui
+p = pular 
+
+Obs: a função len retorna a qtd de caracteres da str
+"""
+
+variavel = 'Olá Mundo'
+
+print(len(variavel[4:7]))
