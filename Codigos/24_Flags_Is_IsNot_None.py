0bd2fddb5a40da08fde0bb1db8774d1703995626
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-09-26 16:48:37 -0300

    None, Is , and is not

---
+++ Codigos/24_Flags_Is_IsNot_None.py
@@ -0,0 +1,20 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não é um valor
+is e is not = é ou não é (tipo, valor, identidade)
+"""
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no IF')
+
+if passou_no_if is not None:
+    print('Passou no IF')
