1d238cea0d5ce09154c451b61019e331e3de0bfd
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-09-27 23:16:31 -0300

    While

---
+++ Codigos/25_metodos_imutaveis.py
@@ -0,0 +1,8 @@
+"""
+Imutáveis: Str, int, float e bool.
+"""
+
+string = "fernando Marques"
+string_alt = f'{string[:3]}abc{string[2:]}'
+print(string_alt)
+print(string.capitalize())
---
+++ Codigos/28_while.py
@@ -0,0 +1,17 @@
+"""
+
+Em Python, o while é uma estrutura de controle de fluxo que permite que você execute um bloco de código repetidamente enquanto uma condição específica for verdadeira. O while é conhecido como um loop de controle de fluxo, pois ele cria um loop que continua executando enquanto a condição especificada for avaliada como verdadeira. Assim que a condição se tornar falsa, a execução do loop será interrompida, e o programa continuará a partir do ponto após o loop.
+
+While = Enquanto
+
+"""
+condicao = True
+
+while condicao:
+    nome = input('Digite seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
