c77993df73ecc3f4d696c904d830244ecdc0f7db
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-10-02 23:28:58 -0300

    While in While

---
+++ Codigos/31_while_while.py
@@ -0,0 +1,17 @@
+qtd_linha = 5
+qtd_coluna = 5
+
+linha = 1
+
+while linha <= qtd_linha:
+    
+    coluna = 1
+    print(linha)
+
+    while coluna <= qtd_coluna:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+
+    linha += 1
+
+print('Fim')
