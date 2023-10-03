bc764c63e83ce2faef40569eff674ac80fb88b2c
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-10-02 23:57:48 -0300

    Ex_While_String

---
+++ Exercicios/08_ex_while_string.py
@@ -0,0 +1,12 @@
+nome = 'Fernando'
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
