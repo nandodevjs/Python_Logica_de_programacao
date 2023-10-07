2c7c468d44bfbfab55fc0b8137533a8c4c60ff1e
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-10-06 23:57:50 -0300

    Update 32_while_else.py

--- Codigos/32_while_else.py
+++ Codigos/32_while_else.py
@@ -1,16 +1,15 @@
-string = 'Fernando'
+""" while/else """
+string = 'Valor qualquer'
 
 i = 0
-
 while i < len(string):
-
     letra = string[i]
-    print(letra)
 
     if letra == ' ':
         break
 
+    print(letra)
     i += 1
-
 else:
-    print('Não encontrei um espaço na string')
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
