c54d51b457e938b92ad3ba6f39e3b25aafb8f3fb
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-10-09 23:53:26 -0300

    Update 10_while_letras.py

--- Exercicios/10_while_letras.py
+++ Exercicios/10_while_letras.py
@@ -4,10 +4,28 @@ frase = 'O Python é uma linguagem de programação' \
 
 i = 0
 
+
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_Vezes = ''
+
 while i < len(frase):
-
-    letra_atual = frase[i]
     
-    count_letras = frase.count(letra_atual)
+    letra_atual = frase[i]
 
-    print(letra_atual,)
+    if letra_atual == ' ':
+        i += 1
+        continue
+    
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_Vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_Vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
