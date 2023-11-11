a5725adbecd5c481308f339dba7b43cb38439a44
Fernando Marques <68031772+nandodevjs@users.noreply.github.com>
2023-11-10 23:54:56 -0300

    Ex Indices

---
+++ Exercicios/12_indices_list.py
@@ -0,0 +1,12 @@
+"""
+Exiba os índices da lista
+0 MARIA
+1 HELENA
+2 LUIZ
+"""
+
+lista = ['MARIA', 'HELENA', 'LUIZ']
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice])
