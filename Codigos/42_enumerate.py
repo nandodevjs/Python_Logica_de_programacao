8ae898d22ce7b1838e5eaeb532f186683b3b6b27
Fernando Marques <68031772+nandodevjs@users.noreply.github.com>
2023-11-12 23:55:31 -0300

    Enumerate

---
+++ Codigos/42_enumerate.py
@@ -0,0 +1,18 @@
+"""
+Enumerate = enumerar iteráveis (índices)
+
+Enumerate numera cada indice da lista/tupla
+
+enumerate(, start=19 )
+
+"""
+
+lista1 = ['Fernando', 'Marques', 'Silva']
+
+#lista_enumerada = enumerate(lista1)
+
+for i in list(enumerate(lista1)):
+#    indice, nome = i
+    print('For da tupla')
+    for n in i:
+        print(f'\t{n}')
